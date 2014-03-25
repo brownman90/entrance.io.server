045d145de4c195e3a3326233448282991b2af1b0
Jan <jan.prill@media-sp.net>
2014-03-25 20:37:54 +0100

    post payload is read

--- src/main/java/io/entrance/verticles/RestApiVerticle.java
+++ src/main/java/io/entrance/verticles/RestApiVerticle.java
@@ -1,15 +1,13 @@
-
 package io.entrance.verticles;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import io.entrance.service.graph.GraphService;
 import io.entrance.service.graph.dsl.Graph;
+import io.entrance.service.json.gson.GSON;
+
+import java.util.Map;
 
 import org.vertx.java.core.Handler;
-import org.vertx.java.core.MultiMap;
+import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.http.HttpServer;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.http.RouteMatcher;
@@ -18,104 +16,137 @@ import org.vertx.java.platform.Verticle;
 import rx.Observable;
 import rx.util.functions.Action1;
 
+import com.google.common.reflect.TypeToken;
+
 public class RestApiVerticle extends Verticle {
 
-    @Override
-    public void start() {
-        super.start();
+	@Override
+	public void start() {
+		super.start();
 
-        HttpServer server = vertx.createHttpServer();
-        RouteMatcher rm = setupRoutes();
-        server.requestHandler(rm);
-        server.listen(8899);
-    }
+		HttpServer server = vertx.createHttpServer();
+		RouteMatcher rm = setupRoutes();
+		server.requestHandler(rm);
+		server.listen(8899);
+	}
 
-    private RouteMatcher setupRoutes() {
-        RouteMatcher matcher = new RouteMatcher();
-        
-        matcher.options("/api/v1/vertex", new Handler<HttpServerRequest>() {
+	private RouteMatcher setupRoutes() {
+		RouteMatcher matcher = new RouteMatcher();
+
+		matcher.options("/api/v1/vertex", new Handler<HttpServerRequest>() {
 			@Override
 			public void handle(HttpServerRequest request) {
-				request.response().headers().add("Access-Control-Allow-Origin", "*");
-				request.response().headers().add("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-				request.response().headers().add("Access-Control-Allow-Headers", "Content-Type");
+				request.response().headers()
+						.add("Access-Control-Allow-Origin", "*");
+				request.response()
+						.headers()
+						.add("Access-Control-Allow-Methods",
+								"PUT, GET, POST, DELETE, OPTIONS");
+				request.response().headers()
+						.add("Access-Control-Allow-Headers", "Content-Type");
 				request.response().end();
 			}
 		});
 
-        /**
-         * VERTICES. READ-operation. GET all vertices. They might be limited by
-         * parameter :limit.
-         */
-        matcher.get("/api/v1/vertex", new Handler<HttpServerRequest>() {
-            @Override
-            public void handle(final HttpServerRequest request) {
-                Observable.from(request).subscribe(new Action1<HttpServerRequest>() {
-                    @Override
-                    public void call(final HttpServerRequest request) {
-                        // request.response().end(new GraphService().readAllVerticesJson());
-                        request.response().headers().add("Access-Control-Allow-Origin", "*");
-                        
-                        request.response().end(new Graph().find().all().json());
-                    }
-                });
-            }
-        });
-        
-        matcher.get("/api/v1/vertex/:id", new Handler<HttpServerRequest>() {
-            @Override
-            public void handle(final HttpServerRequest request) {
-                Observable.from(request).subscribe(new Action1<HttpServerRequest>() {
-                    @Override
-                    public void call(final HttpServerRequest request) {
-                    	request.response().headers().add("Access-Control-Allow-Origin", "*");
-                        request.response().end(new GraphService().readVertexWrapperJson(request.params().get("id")));
-                    }
-                });
-            }
-        });
-        
-        matcher.post("/api/v1/vertex", new Handler<HttpServerRequest>() {
+		/**
+		 * VERTICES. READ-operation. GET all vertices. They might be limited by
+		 * parameter :limit.
+		 */
+		matcher.get("/api/v1/vertex", new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(final HttpServerRequest request) {
+				Observable.from(request).subscribe(
+						new Action1<HttpServerRequest>() {
+							@Override
+							public void call(final HttpServerRequest request) {
+								// request.response().end(new
+								// GraphService().readAllVerticesJson());
+								request.response()
+										.headers()
+										.add("Access-Control-Allow-Origin", "*");
+
+								request.response().end(
+										new Graph().find().all().json());
+							}
+						});
+			}
+		});
+
+		matcher.get("/api/v1/vertex/:id", new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(final HttpServerRequest request) {
+				Observable.from(request).subscribe(
+						new Action1<HttpServerRequest>() {
+							@Override
+							public void call(final HttpServerRequest request) {
+								request.response()
+										.headers()
+										.add("Access-Control-Allow-Origin", "*");
+								request.response().end(
+										new GraphService()
+												.readVertexWrapperJson(request
+														.params().get("id")));
+							}
+						});
+			}
+		});
+
+		matcher.post("/api/v1/vertex", new Handler<HttpServerRequest>() {
 			@Override
 			public void handle(HttpServerRequest request) {
-				Observable.from(request).subscribe(new Action1<HttpServerRequest>() {
-                    @Override
-                    public void call(final HttpServerRequest request) {
-                    	request.response().headers().add("Access-Control-Allow-Origin", "*");
-                    	request.response().headers().add("Access-Control-Allow-Headers", "Content-Type");
-                    	
-                    	MultiMap map = request.params();
-                    	Map<String, Object> properties = new HashMap<String, Object>();
-                    	for (Entry<String, String> entry : map.entries()) {
-                    		properties.put(entry.getKey(), entry.getValue());
-                    	}
-                    	request.response().end(new Graph().create(properties).getNode().json());
-                    }
-                });
+				Observable.from(request).subscribe(
+						new Action1<HttpServerRequest>() {
+							@Override
+							public void call(final HttpServerRequest request) {
+								request.response()
+										.headers()
+										.add("Access-Control-Allow-Origin", "*");
+								request.response()
+										.headers()
+										.add("Access-Control-Allow-Headers",
+												"Content-Type");
+								request.bodyHandler(new Handler<Buffer>() {
+									@Override
+									public void handle(Buffer buffer) {
+										String json = buffer.toString();
+										Map<String, Object> properties = GSON.INSTANCE
+												.gson()
+												.fromJson(
+														json,
+														new TypeToken<Map<String, Object>>() {
+															private static final long serialVersionUID = 1L;
+														}.getType());
+										request.response().end(
+												new Graph().create(properties)
+														.getNode().json());
+									}
+								});
+							}
+						});
 			}
-        });
+		});
 
-        /**
-         * VERTICES. CREATE-operation. POST a new vertex to the collection of
-         * vertices.
-         */
-        matcher.post("/api/v1/vertex/:id", new Handler<HttpServerRequest>() {
-            @Override
-            public void handle(final HttpServerRequest request) {
+		/**
+		 * VERTICES. CREATE-operation. POST a new vertex to the collection of
+		 * vertices.
+		 */
+		matcher.post("/api/v1/vertex/:id", new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(final HttpServerRequest request) {
 
-            }
-        });
+			}
+		});
 
-        /**
-         * VERTEX. DELETE-operation. DELETE the specified vertex (:id).
-         */
-        matcher.delete("/api/v1/vertex/:id", new Handler<HttpServerRequest>() {
-            @Override
-            public void handle(final HttpServerRequest request) {
+		/**
+		 * VERTEX. DELETE-operation. DELETE the specified vertex (:id).
+		 */
+		matcher.delete("/api/v1/vertex/:id", new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(final HttpServerRequest request) {
 
-            }
-        });
+			}
+		});
 
-        return matcher;
-    }
+		return matcher;
+	}
 }
