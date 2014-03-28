844ac30d4a4cbd8a763174a4445b580d0447210c
Jan <jan.prill@media-sp.net>
2014-03-28 23:37:18 +0100

    lay out the crud operations on the realtime board

--- src/main/java/io/entrance/verticles/BusVerticle.java
+++ src/main/java/io/entrance/verticles/BusVerticle.java
@@ -48,9 +48,100 @@ public class BusVerticle extends Verticle {
          }
       });
 
-      /*
-       * Register a handler to deliver-news. The handler broadcasts
-       * received messages afterwards to all subscribers.
+      /**
+       * Register a handler to deliver-news. The handler broadcasts received
+       * messages afterwards to all subscribers.
+       * 
+       * The stucture of data is simply lists in lists:
+       * 
+       * <pre>
+       *   <ul>
+       *     <li class="vertex" data-vertex-id="111">
+       *       <h2>Hey Google is really building this selfdriving car!</h2>
+       *       <p>
+       *         Believe it or not, on my way to work I've seen one of it. There still
+       *         is a driver in there for safety measures, but I've seen it with my own
+       *         eyes: This thing drives itself. Crazy!
+       *       </p>
+       *     </li>
+       *     <li class="vertex" data-vertex-id="222">
+       *       <h2>Superdeveloper needed!</h2>
+       *       <p>
+       *         We'd like to find a new member of our development team. But the
+       *         'War for talent' is really heating up. There is no single developer
+       *         who has yet answered on our advertisement on the job board of the 
+       *         university. Any ideas?
+       *       </p>
+       *       <!-- Nested is now a list of sub-vertices. Which are related to vertex-id 222: -->
+       *       <ul class="sub-vertices">
+       *         <li class="vertex" data-vertex-id="223">
+       *           <div class="meta">
+       *             Relation: 'comment' | created_at: yesterday | by: 'Pit'
+       *           </div>
+       *           <p>
+       *             You might have a look at linked in to find candidates.
+       *           </p>
+       *         </li>
+       *         <li class="vertex" data-vertex-id="224">
+       *           <div class="meta">
+       *             Relation: 'proof-of-concept' | created_at: yesterday | by: 'Jan'
+       *           </div>
+       *           <p>
+       *             Hey Pit. Searching on 'Linked in' isn't what you like to call innovative, right?
+       *             Developers are hanging around Github as far as I know. That's where they are geeking
+       *             around develop on ideas together and have fun. That's where we should have a look.
+       *           </p>
+       *         </li>
+       *       </ul>
+       *     </li>
+       *   </ul>
+       * </pre>
+       * 
+       * Looking at the structure above, the following might happen:
+       * 
+       * <pre>
+       *   <ul>
+       *     <li>
+       *       A new root vertex might be added. This is a piece of information unrelated to already published information.
+       *       JSON-DRAFT:
+       *       {
+       *         id: 444, // just created
+       *         transaction: 'new',
+       *         ... // no relation
+       *       }
+       *     </li>
+       *     <li>
+       *       A new sub-vertex might be related to an existing vertex. E.g. a comment on an already published vertex.
+       *       JSON-DRAFT:
+       *       {
+       *         id: 555, // just created
+       *         transaction: 'new',
+       *         relation: {
+       *           out: 111,
+       *           label: 'comment',
+       *           in: 555
+       *         }
+       *       }
+       *     </li>
+       *     <li>
+       *       An existing vertex might be updated.
+       *       JSON-DRAFT:
+       *       {
+       *         id:111,
+       *         transaction: 'update',
+       *         properties: {
+       *           name: 'my vertex',
+       *           title: 'This is the updated title',
+       *           body: 'The body needed some updated as well',
+       *           ...
+       *         }
+       *       }
+       *     </li>
+       *     <li>
+       *       An existing vertex might be deleted.
+       *     </li>
+       *   </ul>
+       * </pre>
        */
       registerHandler(eb, "io.entrance.deliver-news");
 
